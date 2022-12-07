74371ccbf4122b2356c554aee62dc0b5bc6e9da6
danielaguileta10 <daniel.aguileta@unah.hn>
2022-12-06 22:54:43 -0600

    edit

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -70,7 +70,7 @@
           <div class="col-6">
             <div class="form-group">
                 <label for="">Ingreso Cliente</label>
-                <input type="date" class="form-control" id="ingreso_cliente" name="ingreso_cliente" placeholder="" value="{{old('rtn', $clientes['FECHA_INGRESO_CLIENTE'])}}">
+                <input type="date" class="form-control" id="ingreso_cliente" name="ingreso_cliente" placeholder="" value="{{old('ingreso_cliente', $clientes['FECHA_INGRESO_CLIENTE'])}}">
                 @error('ingreso_cliente')
                 <span class="text-danger">{{$message}}</span>
                 @enderror
